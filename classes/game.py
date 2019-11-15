20054ac29dd5e5792c6e3a6404bc4ce364b92eda
aref maddah <arefmaddah@gmail.com>
2019-11-15 16:45:13 +0330

    after 2

---
+++ classes/game.py
@@ -0,0 +1,43 @@
+import random
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92n'
+    WARNING = '\033[93n'
+    FAIL = '\033[91n'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4n'
+
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk-10
+        self.atkh = atk+10
+        self.df = df
+        self.magic = magic
+        self.actions = ["Attack", "Magic"]
+
+
+def generate_damage(self):
+    return random.randrange(self.atkl, self.atkh)
+
+
+def generate_spell_damage(self, i):
+    mgl = self.magic[i]["dmg"]-5
+    mgh = self.magic[i]["dmg"]+5
+    return random.randrange(mgl, mgh)
+
+
+def take_damage(self, dmg):
+    self.hp -= dmg
+    if self.hp < 0:
+        self.hp = 0
+
+    return self.hp
